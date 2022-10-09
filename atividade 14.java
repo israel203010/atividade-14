92629b1d632cdc2a9222f9df6743dfe43f416c68
israel203010 <111544955+israel203010@users.noreply.github.com>
2022-10-08 21:29:18 -0300

    Add files via upload

---
+++ atividade 14.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner israel = new Scanner (System.in);
+    int potencia = 0;
+    int mulher = 0 ;
+    int homem = 0;
+
+    for (int z = 0; z < 2; z++) {
+    System.out.println("informe o seu sexo(2= feminino) (2 = Masculino");
+      int x = israel.nextInt();
+      System.out.println("informe seu peso kg"):
+      int y = israel.nextInt();
+
+      if (x == 1   y >= 50   y  >= 7  0 ) 
+      
+  
+  
+  
+  
+  
+  }}
